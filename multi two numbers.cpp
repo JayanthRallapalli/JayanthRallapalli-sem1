f5171fda52a60d261100b18531785d5deb1891a6
JayanthRallapalli <rallapallijayanth@gmail.com>
2025-12-03 18:59:14 +0530

    Add files via upload

---
+++ multi two numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,multi;
+	printf("enter the num1:");
+	scanf("%d",&num1);
+	printf("enter the num2:");
+	scanf("%d",&num2);
+	multi = num1*num2;
+	printf("multi = %d\n",multi);
+	return 0;
+}
